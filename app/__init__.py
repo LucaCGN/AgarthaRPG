f77fbdbc51f4c19730504d55296ad478ada4238a
LucaCGN <caco.nonino@outlook.com.br>
2023-09-05 01:15:07 -0300

    Backend Adjustment

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,8 @@
-from flask import Flask, session
+from flask import Flask, redirect, url_for  # Added redirect, url_for
 from config import *
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key_here'  # For session handling
     # Initialize MySQL configurations
     app.config['MYSQL_HOST'] = MYSQL_HOST
     app.config['MYSQL_DB_NAME'] = MYSQL_DB_NAME
@@ -11,13 +10,15 @@ def create_app():
     app.config['MYSQL_USER'] = MYSQL_USER
     app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
 
+    @app.route('/')
+    def index():
+        return redirect(url_for('login_page'))  # Redirect to login page if not logged in
+
     from .routes import api as api_blueprint  # Move the import here to avoid circular import
     app.register_blueprint(api_blueprint, url_prefix='/api')  # Register the blueprint
 
-    @app.route('/')
-    def index():
-        if 'email' in session:
-            return redirect(url_for('some_logged_in_page'))
-        return redirect(url_for('login_page'))
-
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
