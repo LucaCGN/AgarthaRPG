95fba861ecafcd514284f27932caf41b718a3b80
LucaCGN <caco.nonino@outlook.com.br>
2023-09-07 21:20:17 -0300

    gj

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ import os
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='templates', static_folder='static')
     app.secret_key = 'your_secret_key_here'  # Add this line
     print("Current Working Directory:", os.getcwd())
     print("Template Folder:", app.template_folder)
